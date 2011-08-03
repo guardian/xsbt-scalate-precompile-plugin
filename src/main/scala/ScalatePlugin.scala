d49004928a2a3f955b8d3c9b81ca8f488a5fe029
Graham Tackley <graham.tackley@guardian.co.uk>
2011-08-03 16:08:36 +0100

    Basic compilation happening, but uri's are incorrect

--- src/main/scala/ScalatePlugin.scala
+++ src/main/scala/ScalatePlugin.scala
@@ -11,47 +11,40 @@ import java.io.File
 
 object ScalatePlugin extends Plugin {
 
-  val scalateTemplateDirectories = SettingKey[Seq[File]]("scalate-template-directories",
-                                                         "Locations of template files.")
-  val scalateLoggingConfig = SettingKey[File]("scalate-logging-config",
-                                              "Logback config to get rid of that infernal debug output.")
+
+  val scalateTemplateDirectories = SettingKey[Seq[File]]("scalate-template-directories", "Directories containing template files")
+  val scalateTemplates = SettingKey[PathFinder]("scalate-templates", "Full set of template files to process")
+  val scalateLoggingConfig = SettingKey[File]("scalate-logging-config", "Logback config to get rid of that infernal debug output")
+  val scalateFileTypes = SettingKey[FileFilter]("scalate-file-types", "File types processable by Scalate")
 
   private def scala(template: File, outputdir: File) =
     new File(outputdir, "/%s.scala".format(template.getName.replaceAll("[.]", "_")))
 
-  private def recognized(template: File) =
-    TemplateEngine.templateTypes.filter(template.getName.endsWith(_)).size > 0
-
   private def updated(template: File, scala: File) =
     (! scala.exists()) || (template.lastModified > scala.lastModified)
-
-  private def changed(template: File, outputdir: File) =
-    recognized(template) && updated(template, scala(template, outputdir))
 
   private def code(engine: TemplateEngine, template: File) = {
     val source = TemplateSource.fromFile(template, template.getName)
     source.engine = engine
-    source
     engine.generateScala(source).source
   }
 
-  private def generate (engine: TemplateEngine, template: File, outputdir: File, log: Logger) = {
-    log.info(" compiling template: " + template)
-    IO.write(scala(template, outputdir), code(engine, template))
+  private def lazyGenerate(engine: TemplateEngine, template: File, outputdir: File, log: Logger) = {
+    val scalaFile = scala(template, outputdir)
+    if (updated(template, scalaFile)) {
+      log.info(" compiling template: " + template)
+      IO.write(scalaFile, code(engine, template))
+    }
+    scalaFile
   }
 
-  private def scalateLoggingConfigValue: Initialize[File] =
-    (resourceDirectory in Compile) { (d) => new File(d, "/logback.xml") }
-
-  def scalateTemplateDirectoriesValue: Initialize[Seq[File]] =
-    (resourceDirectory in Compile) { (d) => Seq(d) }
-
   def scalateSourceGeneratorTask: Initialize[Task[Seq[File]]] =
-    (streams, sourceManaged in Compile, scalateTemplateDirectories in Compile, scalateLoggingConfig in Compile) map {
+    (streams, sourceManaged in Compile, scalateTemplates in Compile, scalateLoggingConfig in Compile) map {
       (out, outputDir, inputDirs, logConfig) => generateScalateSource(out, outputDir, inputDirs, logConfig)
     }
 
-  def generateScalateSource(out : TaskStreams, outputDir : File, inputDirs : Seq[File], logConfig : File) = {
+  def generateScalateSource(out: TaskStreams, outputDir: File, inputFiles: PathFinder, logConfig : File) = {
+    out.log.info("generate scalate source: out: %s in: %s" format (outputDir, inputFiles.get) )
     // If we throw an exception here, it'll break the compile. Which is what
     // I want.
 
@@ -60,23 +53,20 @@ object ScalatePlugin extends Plugin {
     val engine = new org.fusesource.scalate.TemplateEngine()
     engine.packagePrefix = ""
 
-    for (dir <- inputDirs)
-      if (dir.listFiles != null)
-        for (template <- dir.listFiles.filter(changed(_, outputDir)))
-          generate(engine, template, outputDir, out.log)
-
-    outputDir.listFiles match {
-      case null => Seq()
-      case (files) => files.toList
-    }
+    for (template <- inputFiles.get) yield
+      lazyGenerate(engine, template, outputDir, out.log)
   }
 
   // Overriding settings means these expressions will be automatically interpolated
   // into the project's build.sbt
 
   override def settings = Seq (
-    scalateLoggingConfig in Compile <<= scalateLoggingConfigValue,
-    scalateTemplateDirectories in Compile <<= scalateTemplateDirectoriesValue,
+    scalateLoggingConfig in Compile <<= (resourceDirectory in Compile) { _ / "logback.xml" },
+    scalateFileTypes := TemplateEngine.templateTypes map { t => "*." + t : FileFilter } reduceLeft { _ || _ },
+    scalateTemplateDirectories in Compile <<= (resourceDirectory in Compile) { Seq(_) },
+    scalateTemplates in Compile <<= (scalateTemplateDirectories in Compile, scalateFileTypes) {
+      (dirs, scalateFiles) => dirs ** scalateFiles
+    },
     sourceGenerators in Compile <+= scalateSourceGeneratorTask
   )
 
