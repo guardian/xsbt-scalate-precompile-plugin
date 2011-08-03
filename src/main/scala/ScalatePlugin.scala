659ae64f4adef576b56c8da91fe415d01f82b8fd
Graham Tackley <graham.tackley@guardian.co.uk>
2011-08-03 17:32:09 +0100

    Make scalate plugin work with subdirectories

--- src/main/scala/ScalatePlugin.scala
+++ src/main/scala/ScalatePlugin.scala
@@ -13,48 +13,44 @@ object ScalatePlugin extends Plugin {
 
 
   val scalateTemplateDirectories = SettingKey[Seq[File]]("scalate-template-directories", "Directories containing template files")
-  val scalateTemplates = SettingKey[PathFinder]("scalate-templates", "Full set of template files to process")
-  val scalateLoggingConfig = SettingKey[File]("scalate-logging-config", "Logback config to get rid of that infernal debug output")
   val scalateFileTypes = SettingKey[FileFilter]("scalate-file-types", "File types processable by Scalate")
+  val scalateTemplates = SettingKey[Seq[(File, String)]]("scalate-templates", "Full set of template files to process")
+  val scalateLoggingConfig = SettingKey[File]("scalate-logging-config", "Logback config to get rid of that infernal debug output")
 
-  private def scala(template: File, outputdir: File) =
-    new File(outputdir, "/%s.scala".format(template.getName.replaceAll("[.]", "_")))
+  private def scala(uri: String, outputdir: File) = outputdir / (uri + ".scala")
 
-  private def updated(template: File, scala: File) =
-    (! scala.exists()) || (template.lastModified > scala.lastModified)
+  private def updated(template: File, scala: File) = (!scala.exists()) || (template.lastModified > scala.lastModified)
 
-  private def code(engine: TemplateEngine, template: File) = {
-    val source = TemplateSource.fromFile(template, template.getName)
+  private def code(engine: TemplateEngine, template: File, uri: String) = {
+    val source = TemplateSource.fromFile(template, uri)
     source.engine = engine
     engine.generateScala(source).source
   }
 
-  private def lazyGenerate(engine: TemplateEngine, template: File, outputdir: File, log: Logger) = {
-    val scalaFile = scala(template, outputdir)
+  private def lazyGenerate(engine: TemplateEngine, template: File, uri: String, outputdir: File, log: Logger) = {
+    val scalaFile = scala(uri, outputdir)
     if (updated(template, scalaFile)) {
-      log.info(" compiling template: " + template)
-      IO.write(scalaFile, code(engine, template))
+      log.info(" compiling template: " + uri)
+      IO.write(scalaFile, code(engine, template, uri))
     }
     scalaFile
   }
 
   def scalateSourceGeneratorTask: Initialize[Task[Seq[File]]] =
     (streams, sourceManaged in Compile, scalateTemplates in Compile, scalateLoggingConfig in Compile) map {
-      (out, outputDir, inputDirs, logConfig) => generateScalateSource(out, outputDir, inputDirs, logConfig)
+      (out, outputDir, templates, logConfig) => generateScalateSource(out, outputDir, templates, logConfig)
     }
 
-  def generateScalateSource(out: TaskStreams, outputDir: File, inputFiles: PathFinder, logConfig : File) = {
-    out.log.info("generate scalate source: out: %s in: %s" format (outputDir, inputFiles.get) )
+  def generateScalateSource(out: TaskStreams, outputDir: File, templateMappings: Seq[(File, String)], logConfig : File) = {
     // If we throw an exception here, it'll break the compile. Which is what
     // I want.
-
     System.setProperty("logback.configurationFile", logConfig.toString)
 
     val engine = new org.fusesource.scalate.TemplateEngine()
     engine.packagePrefix = ""
 
-    for (template <- inputFiles.get) yield
-      lazyGenerate(engine, template, outputDir, out.log)
+    for ( (template, uri) <- templateMappings) yield
+      lazyGenerate(engine, template, uri, outputDir, out.log)
   }
 
   // Overriding settings means these expressions will be automatically interpolated
@@ -65,7 +61,7 @@ object ScalatePlugin extends Plugin {
     scalateFileTypes := TemplateEngine.templateTypes map { t => "*." + t : FileFilter } reduceLeft { _ || _ },
     scalateTemplateDirectories in Compile <<= (resourceDirectory in Compile) { Seq(_) },
     scalateTemplates in Compile <<= (scalateTemplateDirectories in Compile, scalateFileTypes) {
-      (dirs, scalateFiles) => dirs ** scalateFiles
+      (dirs, scalateFiles) => dirs ** scalateFiles x relativeTo(dirs)
     },
     sourceGenerators in Compile <+= scalateSourceGeneratorTask
   )
