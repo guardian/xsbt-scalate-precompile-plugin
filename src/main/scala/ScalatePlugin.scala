57473e4dc501ad9cea445c9cef52f290a3255c65
Graham Tackley <graham.tackley@guardian.co.uk>
2011-08-05 14:25:45 +0100

    Don't include settings automatically
    Follow the (newish) convention of forcing users to include settings
    explicitly.
    
    I use this plugin in a multi project build, only one of which requires
    scalate precompilation, so this makes a lot of sense to me.

--- src/main/scala/ScalatePlugin.scala
+++ src/main/scala/ScalatePlugin.scala
@@ -12,7 +12,7 @@ import java.io.File
 object ScalatePlugin extends Plugin {
   val scalateTemplateDirectories = SettingKey[Seq[File]]("scalate-template-directories", "Directories containing template files")
   val scalateFileTypes = SettingKey[FileFilter]("scalate-file-types", "File types processable by Scalate")
-  val scalateTemplates = SettingKey[Seq[(File, String)]]("scalate-templates", "Full set of template files to process")
+  val scalateTemplates = SettingKey[Seq[(File, String)]]("scalate-templates", "Full set of template files to process, with their resulting uris")
 
   private def scala(uri: String, outputdir: File) = outputdir / (uri + ".scala")
 
@@ -33,8 +33,6 @@ object ScalatePlugin extends Plugin {
     scalaFile
   }
 
-  def scalateSourceGeneratorTask = (streams, sourceManaged in Compile, scalateTemplates in Compile) map generateScalateSource
-
   def generateScalateSource(out: TaskStreams, outputDir: File, templateMappings: Seq[(File, String)]) = {
     // If we throw an exception here, it'll break the compile. Which is what
     // I want.
@@ -45,16 +43,17 @@ object ScalatePlugin extends Plugin {
       lazyGenerate(engine, template, uri, outputDir, out.log)
   }
 
-  // Overriding settings means these expressions will be automatically interpolated
-  // into the project's build.sbt
-
-  override def settings = Seq (
+  // recommended approach nowadays is for a plugin not to insert itself into settings automatically,
+  // but allow user to add in as they require.
+  //  - add "seq(com.zentrope.ScalatePlugin.scalatePrecompileSettings :_*)" to your build.sbt
+  val scalatePrecompileSettings: Seq[Project.Setting[_]] = Seq (
     scalateFileTypes := TemplateEngine.templateTypes map { t => "*." + t : FileFilter } reduceLeft { _ || _ },
     scalateTemplateDirectories in Compile <<= (resourceDirectory in Compile) { Seq(_) },
     scalateTemplates in Compile <<= (scalateTemplateDirectories in Compile, scalateFileTypes) {
       (dirs, scalateFiles) => dirs ** scalateFiles x relativeTo(dirs)
     },
-    sourceGenerators in Compile <+= scalateSourceGeneratorTask
+    sourceGenerators in Compile <+=
+      (streams, sourceManaged in Compile, scalateTemplates in Compile) map generateScalateSource
   )
 
 }
